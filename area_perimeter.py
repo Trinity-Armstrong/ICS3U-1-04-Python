9cc85d51c54ad021b0dbcc2bba4fb70998a61fcf
Ubuntu <ubuntu@ip-172-31-30-137.ec2.internal>
2019-09-13 18:34:14 +0000

    Basic math

---
+++ area_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# created by: Trinity Armstrong
+# created on: September 2019
+# this is program calculates basic math
+
+
+def main():
+    print("If a rectangle has the dimentions:")
+    print("5cm x 3cm")
+    print()
+    print("Area is {}cm^2".format(3*5))
+    print("Perimeter is {}cm".format(3+3+5+5))
+
+
+if __name__ == "__main__":
+    main()
